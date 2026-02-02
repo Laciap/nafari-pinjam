e1c91e482d45e95a947d7b5ce903354aac882cbc
Laciap <mokhamadfaisal11@gmail.com>
2026-02-02 13:24:22 +0700

    Part 5 Done Anjir, YT: Iwan Setiawan

--- app/Filament/Resources/Siswas/Tables/SiswasTable.php
+++ app/Filament/Resources/Siswas/Tables/SiswasTable.php
@@ -2,58 +2,80 @@
 
 namespace App\Filament\Resources\Siswas\Tables;
 
-use Filament\Actions\BulkActionGroup;
-use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\ActionGroup;
+use Filament\Tables\Table;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
-use Filament\Tables\Columns\ImageColumn;
+use Filament\Support\Icons\Heroicon;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Support\Enums\FontWeight;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Table;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\Layout\Grid;
+use Filament\Tables\Columns\Layout\Stack;
 
 class SiswasTable
 {
     public static function configure(Table $table): Table
     {
         return $table
+            ->contentGrid([
+                'xl' => 4,
+                'lg' => 3,
+                'md' => 2,
+            ])
             ->columns([
-                ImageColumn::make('profile_picture')
-                ->label('Foto Profil')
-                ->disk('public')
-                ->imageHeight(72),
-                TextColumn::make('user.name')
-                    ->label('Nama Siswa')
-                    ->searchable()
-                    ->sortable(),
-                
-                TextColumn::make('nisn')
-                    ->label('NISN')
-                    ->searchable(),
-                    TextColumn::make('kelas.name')
-                    ->label('Kelas')
-                    ->searchable()
-                    ->sortable(),
-                TextColumn::make('phone_number')
-                    ->label('No. Telepon')
-                    ->searchable(),
-                TextColumn::make('gender')
-                    ->label('Jenis Kelamin')
-                    ->badge(),
-                
-                TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                Grid::make([
+                    'default' => 1,
+                ])->schema([
+
+                    ImageColumn::make('profile_picture')
+                        ->label('Foto Profil')
+                        ->disk('public')
+                        ->imageSize(200),
+
+                    Stack::make([
+                        TextColumn::make('user.name')
+                            ->label('Nama Siswa')
+                            ->searchable()
+                            ->sortable()
+                            ->weight(FontWeight::Bold),
+
+                        TextColumn::make('nisn')
+                            ->label('NISN')
+                            ->searchable()
+                            ->icon(Heroicon::Identification),
+                        TextColumn::make('kelas.name')
+                            ->label('Kelas')
+                            ->searchable()
+                            ->sortable()
+                            ->icon(Heroicon::BuildingOffice),
+                        TextColumn::make('phone_number')
+                            ->label('No. Telepon')
+                            ->searchable()
+                            ->icon(Heroicon::Phone),
+                        TextColumn::make('gender')
+                            ->label('Jenis Kelamin')
+                            ->badge(),
+                    ])
+
+                ]),
+
+
             ])
             ->filters([
                 //
             ])
             ->recordActions([
                 ViewAction::make(),
-                EditAction::make(),
+
+                ActionGroup::make([
+                   EditAction::make(),
+                DeleteAction::make(), 
+                ]),
+                
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
